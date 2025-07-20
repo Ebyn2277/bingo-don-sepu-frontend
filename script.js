184b65a4fe19bef2cde44a44d32d3d008babc623
Brayan Presiga <presigabrayan3@gmai.com>
2025-07-20 13:19:52 -0500

    Update API_BASE_URL

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.APP_CONFIG = {
-  API_BASE_URL: "http://192.168.20.27:8000/api",
+  API_BASE_URL:
+    "https://protestant-vinni-bingo-don-sepu-66e57ef7.koyeb.app/api",
 };
 
 let formData = null;
