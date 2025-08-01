eade318f3ec0cc390be5b8350c13ffb4a870ef51
Brayan Presiga <presigabrayan3@gmai.com>
2025-08-01 16:35:55 -0500

    Remove auto download and add download buttons for sheets

--- successful-script.js
+++ successful-script.js
@@ -1,11 +1,22 @@
 window.onload = async () => {
   const successData = JSON.parse(localStorage.getItem("successData"));
-  console.log(successData);
 
+  // Add buttons to download PDFs
+  const downloadContainer = document.getElementById(
+    "download-sheets-container"
+  );
+  const buttonsContainerFragment = document.createDocumentFragment();
   for (const [i, pdf] of successData.sheets.entries()) {
-    downloadPDF(pdf.source_url, `combo_${i + 1}.pdf`);
-    await new Promise((resolve) => setTimeout(resolve, 500));
+    const button = document.createElement("button");
+    button.className = "download-button";
+    button.innerHTML = `<i class="fa-solid fa-file-arrow-down"></i>Descargar Cartón ${
+      i + 1
+    }`;
+    button.onclick = () => downloadPDF(pdf.source_url, `combo_${i + 1}.pdf`);
+    buttonsContainerFragment.appendChild(button);
   }
+  downloadContainer.appendChild(buttonsContainerFragment);
+
   localStorage.clear();
 };
 
